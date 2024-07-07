c2f1113d63b6e85757a3d42e2b52a5a3e7a25229
Angel Vanegas <angelman@gmail.com>
2024-07-07 14:11:43 -0400

    missing delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update]
     def index
         @posts = Post.all
     end
@@ -12,7 +13,22 @@ class PostsController < ApplicationController
         redirect_to posts_path
     end
 
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
     private
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
 
     def post_params
         params.require(:post).permit(:caption, :image)
